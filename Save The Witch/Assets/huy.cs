147f6ac561ef4466a57fe0e423eebcc3e3803299
BackStayge <herobrin0572@gmail.com>
2024-06-03 20:55:33 +0300

    test 3

--- Save The Witch/Assets/huy.cs
+++ Save The Witch/Assets/huy.cs
@@ -7,7 +7,7 @@ public class huy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.LogError("Åדמנ קלמ");
     }
 
     // Update is called once per frame
