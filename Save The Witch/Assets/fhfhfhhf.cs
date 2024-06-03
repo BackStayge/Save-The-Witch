a0e39a07a195d0cef07c7cbb9cb42f9619417721
BackStayge <herobrin0572@gmail.com>
2024-06-03 21:21:19 +0300

    Test 5

--- Save The Witch/Assets/fhfhfhhf.cs
+++ Save The Witch/Assets/fhfhfhhf.cs
@@ -7,12 +7,12 @@ public class fhfhfhhf : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //gay
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //gay   
     }
 }
--- Save The Witch/Assets/huy.cs
+++ Save The Witch/Assets/huy.cs
@@ -15,4 +15,10 @@ public class huy : MonoBehaviour
     {
         
     }
+
+
+
+
+
+    //gay
 }
